5506caa4b9fc8de70d27efdeecc46aa93444a7fe
Yasir Sarfraz <rm.muzamil.360@gmail.com>
2025-02-16 23:44:40 +0500

    fix term conditons file path

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSelector } from "react-redux"
 import { LegalPage } from "@/components/legal-page"
-import { termsAndConditionsTranslations } from "@/translations/terms-and-conditions"
+import { termsAndConditionsTranslations } from "../translations/terms-and-conditions"
 import type { RootState } from "@/store/store"
 
 export default function TermsAndConditions() {
