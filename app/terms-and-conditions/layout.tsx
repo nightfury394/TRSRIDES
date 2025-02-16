c9536844217c22aa79fc5dc226bd0cbc7a81064f
Yasir Sarfraz <rm.muzamil.360@gmail.com>
2025-02-16 23:48:59 +0500

    add layout in term and conditions

---
+++ app/terms-and-conditions/layout.tsx
@@ -0,0 +1,8 @@
+import { Navigation } from "@/components/navigation";
+import { Footer } from "@/components/ui/Footer";
+import type { ReactNode } from "react";
+
+export default function TermsLayout({ children }: { children: ReactNode }) {
+  return <><Navigation />{children}<Footer /></>
+}
+
